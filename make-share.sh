11efb96bdd2f614655c48d323dce3fc40a7ee673
Nguyen Anh Quynh <aquynh@gmail.com>
2016-04-28 22:12:51 +0800

    do not rm -rf in make-*.sh

--- make-share.sh
+++ make-share.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf *; cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
 time make -j8
