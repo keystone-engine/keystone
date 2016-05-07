1a44492c19336032972546215524b7edc7912e0f
Nguyen Anh Quynh <aquynh@gmail.com>
2016-05-07 21:47:02 +0800

    regress: correct a comment in x86_nasm.py

--- suite/regress/x86_nasm.py
+++ suite/regress/x86_nasm.py
@@ -18,12 +18,14 @@ class TestX86(regress.RegressTest):
         ks = Ks(KS_ARCH_X86, KS_MODE_32)
         # change the syntax to NASM
         ks.syntax = KS_OPT_SYNTAX_NASM
+
         # compile an instruction in NASM syntax
         encoding, count = ks.asm("mov dword [eax], 0x42424242")
         # Assert the result
         self.assertEqual(encoding, [ 0xc7, 0x00, 0x42, 0x42, 0x42, 0x42 ])
-        # instructions separated by \n, and use # for full line commenter
-        encoding, count = ks.asm("add eax, ebx ;inc ecx this is ignored\ninc edx")
+
+        # instructions separated by \n, and use ; for full line commenter
+        encoding, count = ks.asm("add eax, ebx;inc ecx this comment is ignored\ninc edx")
         # Assert the result
         self.assertEqual(encoding, [ 0x01, 0xd8, 0x42 ])
 
