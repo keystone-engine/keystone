32c13df6f51e2ba2581bd08bc5373bc5df823801
Ingmar Steen <iksteen@gmail.com>
2016-05-10 17:58:05 +0200

    Add nodejs bindings

---
+++ bindings/nodejs/consts/arm.js
@@ -0,0 +1,4 @@
+// For Keystone Engine. AUTO-GENERATED FILE, DO NOT EDIT [arm_const.js]
+module.exports.ERR_ASM_ARM_INVALIDOPERAND = 512
+module.exports.ERR_ASM_ARM_MISSINGFEATURE = 513
+module.exports.ERR_ASM_ARM_MNEMONICFAIL = 514
