8b15d76c1a74460ab259b1626eeac535c6a35b34
Nguyen Anh Quynh <aquynh@gmail.com>
2016-05-14 01:15:38 +0800

    allow to build with DEBUG option for make-share.sh & make-lib.sh

--- make-lib.sh
+++ make-lib.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -ex
 
+if [ -n "$1" ]
+then
+# compile with DEBUG option
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+else
+# default compile
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+fi
+
 time make -j8
 
--- make-share.sh
+++ make-share.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -xe
 
+if [ -n "$1" ]
+then
+# compile with DEBUG option
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+else
+# default compile
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+fi
+
 time make -j8
