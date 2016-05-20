1bf32895920e9d7b99db8bf90fe02020e0747135
Nguyen Anh Quynh <aquynh@gmail.com>
2016-05-21 00:05:01 +0800

    regress: add AT&T test to x86_call5.py

--- suite/regress/x86_call5.py
+++ suite/regress/x86_call5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Test 'jmp 0' with both X86 Intel & ATT syntax
+# Test 'call 5' with both X86 Intel & ATT syntax
 
 # Github issue: #56
 # Author: Nguyen Anh Quynh
@@ -8,6 +8,16 @@
 from keystone import *
 
 import regress
+
+class TestX86ATT(regress.RegressTest):
+    def runTest(self):
+        # Initialize Keystone engine
+        ks = Ks(KS_ARCH_X86, KS_MODE_32)
+        # Assemble to get back insn encoding & statement count
+        encoding, count = ks.asm(b"call 5", 0x2000)
+        # Assert the result
+        self.assertEqual(encoding, [ 0xe8, 0x00, 0xe0, 0xff, 0xff ])
+
 
 class TestX86Intel(regress.RegressTest):
     def runTest(self):
