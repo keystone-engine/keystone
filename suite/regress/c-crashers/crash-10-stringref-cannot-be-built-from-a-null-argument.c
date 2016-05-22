bb60851298e44ba9aa77ade7eabf21af4bf076e6
practicalswift <practicalswift@users.noreply.github.com>
2016-05-22 17:19:47 -0400

    Add 13 assertion failure crashers.

---
+++ suite/regress/c-crashers/crash-10-stringref-cannot-be-built-from-a-null-argument.c
@@ -0,0 +1,12 @@
+#include <keystone/keystone.h>
+int main(int argc, char **argv) {
+  int ks_arch = KS_ARCH_SYSTEMZ, ks_mode = KS_MODE_LITTLE_ENDIAN;
+  ks_engine *ks;
+  ks_err err = ks_open(ks_arch, ks_mode, &ks);
+  if (!err) {
+    size_t count, size;
+    unsigned char *insn;
+    ks_asm(ks, 0, 0, &insn, &size, &count);
+  }
+  ks_close(ks);
+}
