b9942f9ce600f67a5e494dad4797ab64ebf56b46
mrexodia <mr.exodia.tpodt@gmail.com>
2016-05-23 09:29:58 +0200

    added test for issue #10 (push word 0xd)

---
+++ suite/regress/x86_issue10.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Test 'push word 0xd'
+
+# Github issue: #10
+# Author: Duncan (mrexodia)
+
+from keystone import *
+
+import regress
+
+class TestX86(regress.RegressTest):
+    def runTest(self):
+        # Initialize Keystone engine
+        ks = Ks(KS_ARCH_X86, KS_MODE_32)
+        # Assemble to get back insn encoding & statement count
+        encoding1, _ = ks.asm(b"push 0xd")
+        encoding2, _ = ks.asm(b"push word 0xd")
+        # Assert the result
+        self.assertEqual(encoding1, [ 0x6a, 0x0d ])
+        self.assertEqual(encoding2, [ 0x66, 0x6a, 0x0d ])
+
+if __name__ == '__main__':
+    regress.main()
