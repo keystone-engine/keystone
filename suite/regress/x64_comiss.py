e9d3a62f2f9fa12ffd9f521a60f7092933e1fafd
Nguyen Anh Quynh <aquynh@gmail.com>
2016-05-26 12:09:39 +0800

    regress: fix x64_comiss.py

--- suite/regress/x64_comiss.py
+++ suite/regress/x64_comiss.py
@@ -2,7 +2,7 @@
 
 # Test LOOP imm
 
-# Github issue: #95
+# Github issue: #92
 # Author: Nguyen Anh Quynh
 
 from keystone import *
@@ -14,7 +14,7 @@ class TestX86(regress.RegressTest):
         # Initialize Keystone engine
         ks = Ks(KS_ARCH_X86, KS_MODE_64)
 
-        encoding, _ = ks.asm(b"comiss xmm0, xmmword ptr [rip + 0x1ca568a]", 0x07FF6BF90CACF)
+        encoding, _ = ks.asm(b"comiss xmm0, dword ptr [rip + 0x1ca568a]", 0x07FF6BF90CACF)
         self.assertEqual(encoding, [ 0x0F, 0x2F, 0x05, 0x8A, 0x56, 0xCA, 0x01 ])
 
 
