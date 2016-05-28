88403bdd215d01a2fae30ea40a2f0b0b187c776d
practicalswift <practicalswift@users.noreply.github.com>
2016-05-28 12:03:17 -0400

    Run crash cases in deterministic order.

--- suite/regress/c-crashers/run-all.sh
+++ suite/regress/c-crashers/run-all.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-for i in `find . -not -name "*.sh" -type f`; do [ -x $i ] && echo "Run $i" && $i; done
+for i in $(find . -not -name "*.sh" -type f | sort -n); do
+    [ -x $i ] && echo "Run $i" && $i
+done
