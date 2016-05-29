f32cb6e4a6d8a618bef270ed4e8553f4632fa637
practicalswift <practicalswift@users.noreply.github.com>
2016-05-29 16:07:34 -0400

    Simplify crash case.

--- suite/regress/c-crashers/crash-26-x64-indexreg-already-set.c
+++ suite/regress/c-crashers/crash-26-x64-indexreg-already-set.c
@@ -3,9 +3,7 @@ int main(int argc, char **argv) {
   int ks_arch = KS_ARCH_X86, ks_mode = KS_MODE_64;
   unsigned char assembly[] = {
     'c', 'L', '-', 'e', 'c', 'L', '-', 'e', 'c', '*',
-    'c', 'L', '-', 'e', 'e', 'c', '*', 'c', 'L', '-',
-    'e', 'c', '-', 0x82, 'c', '-', 0x82, 'c', 'L', 'c',
-    'L', '-', 'e', 'c', 'L', 'L', 0x00,
+    'c', 'L', '-', 'e', 'e', 'c', '*', 'c', 'L', 0x00,
   };
   ks_engine *ks;
   ks_err err = ks_open(ks_arch, ks_mode, &ks);
