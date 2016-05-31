0143c2674b45b2bfb53b94a3dd357c5fc32a9d2d
practicalswift <practicalswift@users.noreply.github.com>
2016-05-31 15:25:44 -0400

    Clean-up crash cases in order to improve readability.

--- suite/regress/c-crashers/crash-19-arm-unable-to-encode-mcoperand.c
+++ suite/regress/c-crashers/crash-19-arm-unable-to-encode-mcoperand.c
@@ -1,15 +1,13 @@
 #include <keystone/keystone.h>
 int main(int argc, char **argv) {
   int ks_arch = KS_ARCH_ARM, ks_mode = KS_MODE_LITTLE_ENDIAN;
-  unsigned char assembly[] = {
-    'C', 'p', 'S', ' ', 'L', ',', 'f', 0x00,
-  };
+  char *assembly = "CpS L,f";
   ks_engine *ks;
   ks_err err = ks_open(ks_arch, ks_mode, &ks);
   if (!err) {
     size_t count, size;
     unsigned char *insn;
-    if (ks_asm(ks, (char *)assembly, 0, &insn, &size, &count))
+    if (ks_asm(ks, assembly, 0, &insn, &size, &count))
       printf("ERROR: failed on ks_asm() with error = %s, code = %u\n", ks_strerror(ks_errno(ks)), ks_errno(ks));
     ks_free(insn);
   }
