f2e51a51d4257bb536ab1966ef10f1f2032803bd
practicalswift <practicalswift@users.noreply.github.com>
2016-06-01 17:30:34 -0400

    Simplify crash cases.

--- suite/regress/c-crashers/crash-17-arm-invalid-size.c
+++ suite/regress/c-crashers/crash-17-arm-invalid-size.c
@@ -1,21 +1,13 @@
 #include <keystone/keystone.h>
 int main(int argc, char **argv) {
   int ks_arch = KS_ARCH_ARM, ks_mode = KS_MODE_LITTLE_ENDIAN;
-  unsigned char assembly[] = {
-    'L', 'D', 'r', ' ', 'A', 'H', '1', 'I', ',', '=',
-    'j', 'H', 'G', 'Q', ',', '=', '2', ',', '=', 'r',
-    ',', '=', 'G', 'Q', ',', '=', '1', '1', '1', '1',
-    '1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
-    '1', '2', ',', '=', 'e', 'y', ',', '=', '2', ',',
-    '=', 'e', 'Q', ',', '=', 'r', 'Q', ',', '=', 'r',
-    ',', '=', 0x00,
-  };
+  char *assembly = "LDr AH1I,=jHGQ,=2,=r,=GQ,=1111111111111112,=ey,=2,=eQ,=rQ',=r,=";
   ks_engine *ks;
   ks_err err = ks_open(ks_arch, ks_mode, &ks);
   if (!err) {
     size_t count, size;
     unsigned char *insn;
-    if (ks_asm(ks, (char *)assembly, 0, &insn, &size, &count))
+    if (ks_asm(ks, assembly, 0, &insn, &size, &count))
       printf("ERROR: failed on ks_asm() with error = %s, code = %u\n", ks_strerror(ks_errno(ks)), ks_errno(ks));
     ks_free(insn);
   }
--- suite/regress/c-crashers/crash-26-x64-indexreg-already-set.c
+++ suite/regress/c-crashers/crash-26-x64-indexreg-already-set.c
@@ -1,16 +1,13 @@
 #include <keystone/keystone.h>
 int main(int argc, char **argv) {
   int ks_arch = KS_ARCH_X86, ks_mode = KS_MODE_64;
-  unsigned char assembly[] = {
-    '_', '-', '_', '*', 'A', 'L', '-', '_', '*', 'A',
-    'L', 0x00,
-  };
+  char *assembly = "_-_*AL-_*AL";
   ks_engine *ks;
   ks_err err = ks_open(ks_arch, ks_mode, &ks);
   if (!err) {
     size_t count, size;
     unsigned char *insn;
-    if (ks_asm(ks, (char *)assembly, 0, &insn, &size, &count))
+    if (ks_asm(ks, assembly, 0, &insn, &size, &count))
       printf("ERROR: failed on ks_asm() with error = %s, code = %u\n", ks_strerror(ks_errno(ks)), ks_errno(ks));
     ks_free(insn);
   }
