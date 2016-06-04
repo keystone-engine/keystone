c76bab3c1a3e018307d28139db5e3f62e9f07bca
Adrian Herrera <adrian.herrera02@gmail.com>
2016-06-04 15:00:33 +1000

    Added ability to auto generate and install a keystone.pc file
    This is required for when I get around to writing some Haskell bindings :-)

---
+++ pkg-config.pc.cmake
@@ -0,0 +1,7 @@
+Name: ${PROJECT_NAME}
+Description: Keystone assembler engine
+Version: ${KEYSTONE_VERSION_MAJOR}.${KEYSTONE_VERSION_MINOR}
+libdir=${PKG_CONFIG_LIBDIR}
+includedir=${PKG_CONFIG_INCLUDEDIR}
+Libs: ${PKG_CONFIG_LIBS}
+Cflags: ${PKG_CONFIG_CFLAGS}
