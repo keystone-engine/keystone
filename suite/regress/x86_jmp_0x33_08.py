bcd3c14fb237980c9e8d2a9bca9ce5d0081de32a
Nguyen Anh Quynh <aquynh@gmail.com>
2016-07-03 01:37:39 +0800

    x86: FARJMP/FARCALL support Nasm syntax

--- suite/regress/x86_jmp_0x33_08.py
+++ suite/regress/x86_jmp_0x33_08.py
@@ -15,8 +15,13 @@ class TestX86Intel(regress.RegressTest):
         ks = Ks(KS_ARCH_X86, KS_MODE_32)
         # Assemble with zero addr
         try:
+            # Keystone (LLVM) syntax
             encoding, count = ks.asm("ljmp 0x33:08", 0)
             self.assertEqual(encoding, [ 0xea, 0x08, 0x00, 0x30, 0x00 ])
+            # NASM syntax
+            ks.syntax = KS_OPT_SYNTAX_NASM
+            encoding2, count = ks.asm("jmp 0x33:08", 0)
+            self.assertEqual(encoding, encoding2)
         except KsError as e:
             pass
 
