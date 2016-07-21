2fdd772eddfef53681385f9fd8ee5e9590a9a28e
Nguyen Anh Quynh <aquynh@gmail.com>
2016-07-21 19:56:44 +0800

    make.sh: add 2 new options lib_only & lib32

--- make-common.sh
+++ make-common.sh
@@ -7,8 +7,21 @@ ARCH='i386;x86_64'
 # Examples are Fedora, Redhat, Suse.
 LLVM_LIBDIR_SUFFIX=''
 
+# by default we do NOT build 32bit on 64bit system
+LLVM_BUILD_32_BITS=0
+
+# by default we build libraries & kstool
+# but we can skip kstool & build libraries only
+BUILD_LIBS_ONLY=0
+
 while [ "$1" != "" ]; do
   case $1 in
+    lib_only)
+      BUILD_LIBS_ONLY=1
+      ;;
+    lib32)
+      LLVM_BUILD_32_BITS=1
+      ;;
     lib64)
       LLVM_LIBDIR_SUFFIX='64'
       ;;
