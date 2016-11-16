b61a9d2d62eafa2e57d6fc4ff85a231776f515f9
Shakeeb Ahmed <shak@redballoonsecurity.com>
2016-11-16 13:29:20 -0500

    reducing number of instructions required to create failure

--- suite/regress/arm_bl_location.py
+++ suite/regress/arm_bl_location.py
@@ -15,13 +15,9 @@ class TestARM(regress.RegressTest):
         ks = Ks(KS_ARCH_ARM, KS_MODE_ARM)
         # Assemble to get back insn encoding & statement count
         encoding, count = ks.asm(b"""
-                    push {r4, r5, r6, r7, r8, sb, sl, lr}
-                    mov r4, r0
-                    mov r0, r1
-                    mov r6, r1
                     and sb, r2, #0xff
                     lsr r7, r2, #8
-                    bl #0xfffe5078
+                    bl #0xfffe5068
                     """)
         # Assert the result
         self.assertEqual(encoding[:4], [ 0x16, 0x94, 0xff, 0xeb ])
