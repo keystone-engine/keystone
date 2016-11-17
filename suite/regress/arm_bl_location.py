8018a517ceb00c426550df81f1a32e533e918176
Shakeeb Ahmed <shak@redballoonsecurity.com>
2016-11-16 23:34:18 -0500

    Adding more test cases

--- suite/regress/arm_bl_location.py
+++ suite/regress/arm_bl_location.py
@@ -15,7 +15,22 @@ class TestARM(regress.RegressTest):
         ks = Ks(KS_ARCH_ARM, KS_MODE_ARM)
         # Assemble to get back insn encoding & statement count
         encoding, count = ks.asm(b"""
-                    and sb, r2, #0xff
+                    nop
+                    bl #0xfffe5064
+                    """)
+        # Assert the result
+        self.assertEqual(encoding[-4:], [ 0x16, 0x94, 0xff, 0xeb ])
+
+        encoding, count = ks.asm(b"""
+                    nop
+                    nop
+                    bl #0xfffe5068
+                    """)
+        # Assert the result
+        self.assertEqual(encoding[-4:], [ 0x16, 0x94, 0xff, 0xeb ])
+
+        encoding, count = ks.asm(b"""
+                    nop
                     lsr r7, r2, #8
                     bl #0xfffe5068
                     """)
