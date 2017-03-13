6a916acb2040109818c4869695f67e417a8c93bf
Nguyen Anh Quynh <aquynh@gmail.com>
2017-03-13 20:41:51 +0800

    update nmake*.bat on the position of generated libraries

--- nmake-lib.bat
+++ nmake-lib.bat
@@ -2,6 +2,8 @@
 :: Build Keystone static library (keystone.lib) on Windows with CMake & Nmake
 :: By Nguyen Anh Quynh, 2016
 
+:: This generates .\llvm\lib\keystone.lib
+
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DLLVM_TARGETS_TO_BUILD="all" -G "NMake Makefiles" ..
 nmake
 
