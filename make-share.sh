1e8dddb059a858321f4cecd4f8820858388f3ac8
Christian Sharpsten <chris.sharpsten@gmail.com>
2018-03-20 00:32:44 -0400

    Auto-detect install libdir to enable multi-arch debian builds
    When compiling on linux, use GNUInstallDirs to automatically determine
    the appropriate libdir path for multi-arch builds instead of hardcoding
    lib/ or lib64/. GNUInstallDirs should follow the FHS appropriately for
    non-debian platforms that place libs in lib64/. This also removes the
    need to specify `lib64` on the command line when building for a
    non-debian system that follows the linux FHS.
    
    When compiling with cmake directly on debian and specifying
    `-DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_LIBRARY_ARCHITECTURE=<arch>`, the
    libdir should be set to /usr/lib/<arch>/.

--- make-share.sh
+++ make-share.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Build shared library of Keystone Engine
-# syntax: make-share.sh [debug] [macos-no-universal] [lib32] [lib64] [lib_only]
+# syntax: make-share.sh [debug] [macos-no-universal] [lib32] [lib_only]
 
 usage()
 {
@@ -12,12 +12,11 @@ usage()
   echo "         macos-no-universal: do not build MacOS universal binaries"
   echo "         lib_only: skip kstool & only build libraries"
   echo "         lib32: build 32bit libraries on 64bit system"
-  echo "         lib64: install Linux x64 libraries in \$PREFIX/lib64 (Fedora/Redhat/Suse, etc)"
   echo ""
 }
 
 . "$(dirname "$0")"/make-common.sh
 
-cmake -DBUILD_LIBS_ONLY=$BUILD_LIBS_ONLY -DLLVM_BUILD_32_BITS="$LLVM_BUILD_32_BITS" -DLLVM_LIBDIR_SUFFIX="$LLVM_LIBDIR_SUFFIX" -DCMAKE_OSX_ARCHITECTURES="$ARCH" -DCMAKE_BUILD_TYPE=$BUILDTYPE -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
+cmake -DBUILD_LIBS_ONLY=$BUILD_LIBS_ONLY -DLLVM_BUILD_32_BITS="$LLVM_BUILD_32_BITS" -DCMAKE_OSX_ARCHITECTURES="$ARCH" -DCMAKE_BUILD_TYPE=$BUILDTYPE -DBUILD_SHARED_LIBS=ON -DLLVM_TARGETS_TO_BUILD="all" -G "Unix Makefiles" ..
 
 make -j8
