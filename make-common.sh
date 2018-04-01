3e67c1d488ce29a224b5645b8e21880f072eb6f8
Christian Sharpsten <shareef12@twelvetacos.com>
2018-03-31 22:38:32 -0400

    Auto-detect install libdir to enable multi-arch debian builds (#345)
    When compiling on linux, use GNUInstallDirs to automatically determine
    the appropriate libdir path for multi-arch builds instead of hardcoding
    lib/ or lib64/. GNUInstallDirs should follow the FHS appropriately for
    non-debian platforms that place libs in lib64/. This also removes the
    need to specify `lib64` on the command line when building for a
    non-debian system that follows the linux FHS.
    
    When compiling with cmake directly on debian and specifying
    `-DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_LIBRARY_ARCHITECTURE=<arch>`, the
    libdir should be set to /usr/lib/<arch>/.

--- make-common.sh
+++ make-common.sh
@@ -3,10 +3,6 @@ BUILDTYPE='Release'
 # on MacOS, build universal binaries by default
 ARCH='i386;x86_64'
 
-# Linux FHS wants to install x64 libraries in "lib64"
-# Examples are Fedora, Redhat, Suse.
-LLVM_LIBDIR_SUFFIX=''
-
 # by default we do NOT build 32bit on 64bit system
 LLVM_BUILD_32_BITS=0
 
@@ -21,9 +17,6 @@ while [ "$1" != "" ]; do
       ;;
     lib32)
       LLVM_BUILD_32_BITS=1
-      ;;
-    lib64)
-      LLVM_LIBDIR_SUFFIX='64'
       ;;
     debug)
       BUILDTYPE='Debug'
