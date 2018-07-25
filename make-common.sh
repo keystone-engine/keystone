f9239fcd73f0f824dfd1eb7c52c902fc8f987d76
Binyamin Sharet <s.binyamin@gmail.com>
2018-07-25 14:25:33 +0300

    Don't build universal binaries by default
    Starting Xcode 10 beta, there's no support for i386.
    This commit changes the default build on iOS to regular libraries
    instead of universal binaries.
    Build of universal binaries is still supported with macos-universal.

--- make-common.sh
+++ make-common.sh
@@ -1,7 +1,7 @@
 BUILDTYPE='Release'
 
-# on MacOS, build universal binaries by default
-ARCH='i386;x86_64'
+# on MacOS, do not build universal binaries by default
+ARCH=''
 
 # by default we do NOT build 32bit on 64bit system
 LLVM_BUILD_32_BITS=0
@@ -21,8 +21,8 @@ while [ "$1" != "" ]; do
     debug)
       BUILDTYPE='Debug'
       ;;
-    macos-no-universal)
-      ARCH=''	# do not build MacOS universal binaries
+    macos-universal)
+      ARCH='i386;x86_64'	# build MacOS universal binaries
       ;;
     *)
       echo "ERROR: unknown parameter \"$1\""
